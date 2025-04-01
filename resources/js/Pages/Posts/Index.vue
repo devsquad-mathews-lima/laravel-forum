a12a4b811801bc469c96a1581b1286f2f70b9740
Mathews Lima <mathews.lima@devsquad.com>
2025-04-01 10:30:57 -0300

    setup pagination interface and loop through posts

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
 import AppLayout from "@/Layouts/AppLayout.vue";
+import Container from "@/Components/Container.vue";
+import Pagination from "@/Components/Pagination.vue";
+
+interface PaginatedData<T> {
+    data: T[];
+    links: {
+        first: string;
+        last: string;
+        prev: string | null;
+        next: string | null;
+    };
+    meta: {
+        current_page: number;
+        from: number;
+        last_page: number;
+        links: {
+            url: string | null;
+            label: string;
+            active: boolean;
+        }[];
+        path: string;
+        per_page: number;
+        to: number;
+        total: number;
+    };
+}
 
 interface Post {
     id: number;
@@ -11,16 +37,22 @@ interface Post {
 }
 
 defineProps<{
-    posts: Post[]
+    posts: PaginatedData<Post>
 }>()
 </script>
 
 <template>
     <AppLayout>
-        <ul>
-            <li v-for="post in posts" :key="post.id">
-                {{ post.title }}
-            </li>
-        </ul>
+        <Container>
+            <ul class="divide-y">
+                <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
+                    <span class="font-bold text-lg">
+                        {{ post.title }}
+                    </span>
+                </li>
+            </ul>
+
+            <Pagination :meta="posts.meta" />
+        </Container>
     </AppLayout>
 </template>
