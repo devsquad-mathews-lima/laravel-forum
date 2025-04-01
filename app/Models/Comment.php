017246ac491cc37733990a3232dc858196fb2d72
Mathews Lima <mathews.lima@devsquad.com>
2025-04-01 09:12:40 -0300

    add docblock

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,10 +3,36 @@
 namespace App\Models;
 
 use Database\Factories\CommentFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property int $post_id
+ * @property string $body
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Post $post
+ * @property-read User $user
+ *
+ * @method static CommentFactory factory($count = null, $state = [])
+ * @method static Builder<static>|Comment newModelQuery()
+ * @method static Builder<static>|Comment newQuery()
+ * @method static Builder<static>|Comment query()
+ * @method static Builder<static>|Comment whereBody($value)
+ * @method static Builder<static>|Comment whereCreatedAt($value)
+ * @method static Builder<static>|Comment whereId($value)
+ * @method static Builder<static>|Comment wherePostId($value)
+ * @method static Builder<static>|Comment whereUpdatedAt($value)
+ * @method static Builder<static>|Comment whereUserId($value)
+ *
+ * @mixin Eloquent
+ */
 class Comment extends Model
 {
     /** @use HasFactory<CommentFactory> */
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,39 @@
 namespace App\Models;
 
 use Database\Factories\PostFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $body
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Comment> $comments
+ * @property-read int|null $comments_count
+ * @property-read User $user
+ *
+ * @method static PostFactory factory($count = null, $state = [])
+ * @method static Builder<static>|Post newModelQuery()
+ * @method static Builder<static>|Post newQuery()
+ * @method static Builder<static>|Post query()
+ * @method static Builder<static>|Post whereBody($value)
+ * @method static Builder<static>|Post whereCreatedAt($value)
+ * @method static Builder<static>|Post whereId($value)
+ * @method static Builder<static>|Post whereTitle($value)
+ * @method static Builder<static>|Post whereUpdatedAt($value)
+ * @method static Builder<static>|Post whereUserId($value)
+ *
+ * @mixin Eloquent
+ */
 class Post extends Model
 {
     /** @use HasFactory<PostFactory> */
